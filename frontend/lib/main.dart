a0a0681127be2db8fed0a102a7534c45fdc5dc7d
Liam <pbm6967@gmail.com>
2025-11-21 14:39:12 +0900

    front-end fix

---
+++ frontend/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Project A1',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Project A1 Flutter Web"),
+      ),
+      body: const Center(
+        child: Text(
+          "Hello! This is Project A1 Web Frontend!",
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
